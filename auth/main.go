8471a0b017eba1fb7bb3da0e2b0ace8e1feac17e
Pilgrim-Xzed <saeedbello12@gmail.com>
2023-10-31 09:02:28 +0000

    updated master branch

--- auth/main.go
+++ auth/main.go
@@ -33,7 +33,7 @@ import (
 )
 
 func main() {
-	// Load .env
+	// Load .env files
 	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Warn("Error loading .env file (this is fine)", "err", err)
